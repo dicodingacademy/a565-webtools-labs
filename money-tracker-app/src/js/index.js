d7504dd52d11635e765e8494af97b380f4a9e2ac
Nur Rizki Adi Prasetyo <nrizki@dicoding.com>
2023-02-10 09:11:19 +0700

    Add money tracker app finish uidasar dashboard

---
+++ money-tracker-app/src/js/index.js
@@ -0,0 +1,29 @@
+// Import our custom CSS
+import '../scss/main.scss';
+
+// Import javascript file as needed
+import Dashboard from './pages/dashboard';
+import * as bootstrap from 'bootstrap';
+
+const routes = {
+  '/': Dashboard,
+};
+
+const detectRoute = () => routes[window.location.pathname];
+
+const initPages = () => {
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const footer = document.querySelector('footer');
+
+  if (header && main && footer) {
+    main.style.minHeight = `calc(100vh - ${header.clientHeight + footer.clientHeight}px)`;
+  }
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+  initPages();
+
+  const route = detectRoute();
+  route.init();
+});
